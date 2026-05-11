df2dd9ad3f89e49e30673a0e7701061efdcaf908
marvemakuyana <marvellousmakuyana@gmail.com>
2026-05-11 16:06:33 +0200

    added more tests

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,12 @@
 // import Onboarding from "../components/Onboarding";
-import TermsAndConditions from "../components/TermsAndConditions";
+import ExpandableText from "../components/ExpandableText";
+//import TermsAndConditions from "../components/TermsAndConditions";
 
 const PlaygroundPage = () => {
   // return <Onboarding />;
-  return <TermsAndConditions />;
+  return (
+    <ExpandableText text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit necessitatibus omnis dolorem, quo temporibus laudantium maiores pariatur alias doloribus eaque officiis quasi architecto assumenda praesentium modi nihil tempore vitae delectus quam deserunt! Temporibus necessitatibus quod harum inventore vitae nulla quaerat asperiores esse earum neque deleniti ipsa deserunt facilis ducimus illo laboriosam fuga, illum minima eum incidunt blanditiis fugiat totam! Voluptate laudantium nulla vero quis, consequuntur rerum, cupiditate expedita odit dolore, nihil blanditiis explicabo nam cum minima ullam. Natus, odit dolorem? Adipisci reiciendis atque saepe sint, nulla corporis explicabo perferendis iste odit illo? Sunt ducimus dolorem fugit quaerat sequi minima natus." />
+  );
 };
 
 export default PlaygroundPage;
